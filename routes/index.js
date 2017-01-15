a9fe9a5ae5a37b6d748971e5007b1b9865d6b7a6
Serjay Ilaga <serjay_ilaga@yahoo.com>
2017-01-15 11:58:44 +0800

    Modified -1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var data = {
+  	title: 'ExpressVids - The top NodeJS and Express Tutorial'
+  }
+
+  res.render('index', data);
 });
 
 module.exports = router;
