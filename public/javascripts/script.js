06efcdf0d8cab5814d752fe53a4094b73f51b149
Serjay Ilaga <serjay_ilaga@yahoo.com>
2017-01-29 11:10:28 +0800

    Fix web pages

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
  $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 80 // Creates a dropdown of 15 years to control year
   });
  
  $(document).ready(function() {
