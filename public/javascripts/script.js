0665c2e27429b42fad0bdcf05475d2ddbb24b08f
Serjay Ilaga <serjay_ilaga@yahoo.com>
2017-02-07 11:26:20 +0800

    Add modifications to layout and search bar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,11 +11,6 @@ $(document).ready(function(){
 
 });
 
- $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 100 // Creates a dropdown of 15 years to control year
-  });
-
 function validateForm() {
 
 	var checkCount = 0;
